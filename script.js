ead5d512f55371574a920b89968949918df3a7f8
igorv <igorvinson@gmail.com>
2023-03-03 21:24:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const burgerBtn = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger-menu');
+
+burgerBtn.addEventListener('click', () => {
+   burgerMenu.classList.toggle('active');
+});
+
+
